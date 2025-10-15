bf5f1fd3013245c91c804bafdffc2fb01ce6a42d
Trustlypay <support@trustlypay.com>
2025-10-15 16:55:24 +0530

    added payin and payout vendor names

--- src/main/java/com/trustlypay/DashboardApis/Service/MerchantServicesServices.java
+++ src/main/java/com/trustlypay/DashboardApis/Service/MerchantServicesServices.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 
-
 @Service
 public class MerchantServicesServices {
 
@@ -17,20 +16,35 @@ public class MerchantServicesServices {
     }
 
     /**
-     * Get payin/payout status for all merchants along with merchant name
-     *
-     * @return List of merchants with merchantName, payinEnabled, payoutEnabled
+     * Get payin/payout status for all merchants along with merchant name, payin vendor, payout vendor
      */
     public List<Map<String, Object>> getAllMerchantsPayinPayoutStatus() {
-        String sql = "SELECT ms.merchant_id, m.name AS merchantName, ms.payin, ms.payout " +
-                "FROM trustlypay_db.merchant_services ms " +
-                "JOIN trustlypay_db.merchant m ON ms.merchant_id = m.id";
+        String sql = """
+                SELECT 
+                    ms.merchant_id,
+                    m.name AS merchantName,
+                    ms.payin,
+                    ms.payout,
+                    vb.bank_name AS payinVendorName,
+                    pvb.bank_name AS payoutVendorName
+                FROM trustlypay_db.merchant_services ms
+                JOIN trustlypay_db.merchant m ON ms.merchant_id = m.id
+                LEFT JOIN trustlypay_db.merchant_vendor_bank mvb ON mvb.merchant_id = ms.merchant_id
+                LEFT JOIN trustlypay_db.vendor_bank vb ON mvb.upi = vb.id
+                LEFT JOIN trustlypay_db.merchant_payout_vendor mpv ON mpv.merchant_id = ms.merchant_id
+                LEFT JOIN trustlypay_db.payout_vendor_bank pvb ON mpv.imps = pvb.id
+                """;
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> Map.of(
-                "merchantId", rs.getInt("merchant_id"),
-                "merchantName", rs.getString("merchantName"),
-                "payinEnabled", rs.getInt("payin") == 1,
-                "payoutEnabled", rs.getInt("payout") == 1
-        ));
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Map<String, Object> map = new java.util.HashMap<>();
+            map.put("merchantId", rs.getInt("merchant_id"));
+            map.put("merchantName", rs.getString("merchantName"));
+            map.put("payinEnabled", rs.getInt("payin") == 1);
+            map.put("payoutEnabled", rs.getInt("payout") == 1);
+            map.put("payinVendorName", rs.getString("payinVendorName"));  // can be null
+            map.put("payoutVendorName", rs.getString("payoutVendorName")); // can be null
+            return map;
+        });
+
     }
 }
